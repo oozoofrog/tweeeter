876c1f68d079a2c971adb4739150410b4129d47a
rollmind <eyerama@gmail.com>
2018-12-16 01:49:06 +0900

    TweetsViewModel 작성

---
+++ Tweeeter/Feature/Tweets/TweetsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TweetsViewModel.swift
+//  Tweeeter
+//
+//  Created by eyebookpro on 16/12/2018.
+//  Copyright © 2018 ngenii. All rights reserved.
+//
+
+import Foundation
+
+final class TweetsViewModel {
+    
+}
